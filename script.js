e199c67bbf43fbfa17f4a52aa294564d0d1af61b
Farkhodoff21 <farkhodoffcodes@gmail.com>
2022-07-07 16:36:09 +1400

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+    #Masalaning Sharti
+    Berilgan: ('anor')
+    Talab qilingan javob: ('rona')
+*/
+
+function rvs(string) { 
+    return string.reverse()
+    
+}
+
+console.log(rvs('Hello'));
